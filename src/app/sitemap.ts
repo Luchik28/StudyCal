48a079df9a067fa9a6995aeb44d72b2d0f11ce25
Luchik28 <28taidan@hawken.edu>
2025-07-14 18:20:30 -0400

    fixed

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -27,12 +27,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.5,
-    },
-    {
-      url: `${baseUrl}/sitemap`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.3,
     }
   ]
 }
